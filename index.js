ccf8c7df23951417d0b59316f063ec0cc026a5d2
KEN <kilol@qq.com>
2016-10-10 17:17:07 +0800

    修复分页页码错误，增强分页功能。加入单页应用多分页模块功能

--- index.js
+++ index.js
@@ -1,29 +1,95 @@
-
 var avalon = require('avalon2')
 require('./style.scss')
+function getHashStr(name) {
+    var url = location.hash; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?")) {
+        var str = url.substr(url.indexOf("?") + 1);
+        var strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest[name];
+}
+function getPages(currentPage) {
+    var pages = []
+    var s = this.showPages
+    var total = this.totalPages
+    var half = Math.floor(s / 2)
+    var start = currentPage - half + 1 - s % 2
+    var end = currentPage + half
+    
+    // handle boundary case
+    if (start <= 0) {
+        start = 1;
+        end = s > total ? total : s;
+    }
+    if (end > total) {
+        start = total - s + 1
+        end = total
+    }
+    
+    var itPage = start;
+    while (itPage <= end) {
+        pages.push(itPage)
+        itPage++
+    }
+    
+    return {currentPage: currentPage, pages: pages};
+}
 avalon.component('ms-pager', {
     template: require('./template.icon.html'),
     defaults: {
-        getHref: function (a) {
-            return '#page-' + this.toPage(a)
+        /*默认ID*/
+        $id        : 'ms-pager',
+        /**
+         * 配置复杂型单页
+         * 默认为false
+         * 如#/page=2
+         * hash配置子页，后面还需配置分页信息时启用
+         * 如#/game?page=2
+         * 表示单页应用进入子页面game页,当前页为2
+         * */
+        is_more    : false,
+        getHref    : function (a) {
+            if (this.is_more) {
+                if (location.hash) {
+                    var search = location.hash,
+                        page   = getHashStr('page');
+                    if (page) { // 存在就替换
+                        search = search.replace('page=' + page, 'page=' + this.toPage(a));
+                        return search;
+                    }
+                    else { // 不存在就叠加
+                        return location.hash + '?page=' + this.toPage(a)
+                    }
+                }
+                else {
+                    return '#?page=' + this.toPage(a)
+                }
+            }
+            else {
+                return '#page-' + this.toPage(a)
+            }
         },
-        getTitle: function (title) {
+        getTitle   : function (title) {
             return title
         },
-        isDisabled: function (name, page) {
+        isDisabled : function (name, page) {
             return this.$buttons[name] = (this.currentPage === page)
         },
-        $buttons: {},
-        showPages: 5,
-        pages: [],
-        totalPages: 15,
+        $buttons   : {},
+        showPages  : 5,
+        pages      : [],
+        totalPages : 1,
         currentPage: 1,
-        firstText: 'First',
-        prevText: 'Previous',
-        nextText: 'Next',
-        lastText: 'Last',
+        firstText  : '首页',
+        prevText   : '上一页',
+        nextText   : '下一页',
+        lastText   : '最后一页',
         onPageClick: avalon.noop,
-        toPage: function (p) {
+        toPage     : function (p) {
             var cur = this.currentPage
             var max = this.totalPages
             switch (p) {
@@ -39,70 +105,69 @@ avalon.component('ms-pager', {
                     return p
             }
         },
-
+        
         cbProxy: function (e, p) {
-            var cur = this.toPage(p);
-            this.render(cur);
-            if (this.$buttons[p] || p === this.currentPage) { // 调整顺序然页码正常显示
-                e.preventDefault()
-                return //disabled, active不会触发
-            }
-            return this.onPageClick(e, cur);
-        },
-        render: function(cur){
-            var obj = getPages.call(this, cur)
             var that = this;
-            setTimeout(function () { // 修复
-                that.currentPage = obj.currentPage;
-                that.pages = obj.pages;
+            /**
+             * 最关键修复
+             * 延迟页码变更，
+             * 防止当pages改变时，
+             * 导致点击href不一致
+             * */
+            setTimeout(function () {
+                var cur = that.toPage(p);
+                if (that.$buttons[p] || p === that.currentPage) {
+                    if (cur === 1) {
+                        return that.onPageClick(e, cur);
+                    }
+                    e.preventDefault()
+                    return //disabled, active不会触发
+                }
+                that.render(cur);
+                return that.onPageClick(e, cur);
             }, 4)
         },
-        rpage: /(?:#|\?)page\-(\d+)/,
-        onInit: function () {
-            var cur = this.currentPage
-            var match = this.rpage && location.href.match(this.rpage)
+        render : function (cur) {/*更新页码*/
+            var obj = getPages.call(this, cur);
+            console.log(cur);
+            this.currentPage = obj.currentPage;
+            this.pages = obj.pages;
+        },
+        /*此处供正常单页应用*/
+        rpage  : function () {
+            return this.is_more ? /(?:#|\?)page\=(\d+)/ : /(?:#|\?)page\-(\d+)/;
+        },
+        cur    : function () { /*正确获取匹配页码*/
+            var cur = this.currentPage;
+            var match = this.rpage && location.href.match(this.rpage());
+            console.log(location.href.match(this.rpage()));
             if (match && match[1]) {
                 var cur = ~~match[1]
                 if (cur < 0 || cur > this.totalPages) {
                     cur = 1
                 }
             }
-            var that = this
-            this.$watch('totalPages', function(){
-                setTimeout(function(){
-                    that.render(that.currentPage)
-                },4)
-            })
-            this.cbProxy(window.event, cur);
+            return cur;
+        },
+        onInit : function () {
+            var that = this;
+            this.$watch('totalPages', function () {
+                /**复杂单页应用，切换选项卡，重置页码
+                 * 但切换选项卡或者数据页数变化时，重置页码
+                 * */
+                that.render(that.cur())
+            });
+            this.$watch('currentPage', function () {
+                /**复杂单页应用，切换选项卡，重置页码
+                 * 切换选项卡，从页码1开始
+                 * */
+                that.render(that.cur());
+            });
+            /*进入页面预载入页码*/
+            that.render(that.cur());
         }
     }
 })
-function getPages(currentPage) {
-    var pages = []
-    var s = this.showPages
-    var total = this.totalPages
-    var half = Math.floor(s / 2)
-    var start = currentPage - half + 1 - s % 2
-    var end = currentPage + half
-
-    // handle boundary case
-    if (start <= 0) {
-        start = 1;
-        end = s > total ? total : s;
-    }
-    if (end > total) {
-        start = total - s + 1
-        end = total
-    }
-
-    var itPage = start;
-    while (itPage <= end) {
-        pages.push(itPage)
-        itPage++
-    }
-
-    return {currentPage: currentPage, pages: pages};
-}
 
 
 
